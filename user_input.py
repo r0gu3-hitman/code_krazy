eae9071c038fc88dae7019fb204a925ed14aee7d
r0gu3-hitman <packetmonger23@gmail.com>
2022-09-26 23:18:25 -0400

    first commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+##This code is used to examine basic user input
+
+likes_snakes = input("Do you like snakes? ")
+print(likes_snakes)
+
+print("Awesome, so do I!")
+favorite_snake = input("Which species is your favotite snake?. ")
+
+print(favorite_snake +"s " + " are wonderful!")
